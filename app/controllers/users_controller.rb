6bdb13809801c979442f12f0aaa8a46a80facf5b
Yuki-Ichigo <bzpl1215@gmail.com>
2020-07-05 19:00:31 +0900

    t.users edit機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,25 @@ class UsersController < ApplicationController
 		@user = current_user
 	end
 
+	def create
+
+	end
+
 	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update(user_params)
+		redirect_to users_path(user.id)
 	end
 
 	def show
 	end
+
+	private
+    def user_params
+    params.require(:user).permit(:name, :introduction)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   root 'home#top'
   get '/about' => 'home#about'
+
 end
