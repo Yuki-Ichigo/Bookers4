501d033dac407d9bc11268f8499cb891f9e4022e
Yuki-Ichigo <bzpl1215@gmail.com>
2020-07-05 19:35:33 +0900

    画像機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d9e904788bfe16dd9b01ac434a3022fe3297fa7bd3cb35b703138addc046962ab1bba652328b77a6c95ba03617ff69fe332990a7c58be2e54a94b6350c9a805'
